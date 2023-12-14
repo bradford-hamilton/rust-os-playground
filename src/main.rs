5cd022ad71a701e1b7e7e48b36ec7d017ae5c9d3
bradford lamson-scribner <brad.lamson@gmail.com>
2023-12-14 16:16:33 -0700

    feat: create allocator mod and get a heap working

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,15 @@
 #![test_runner(rust_os_playground::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+extern crate alloc;
+
+use alloc::{boxed::Box, rc::Rc, vec, vec::Vec};
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
-use rust_os_playground::{memory::BootInfoFrameAllocator, println};
+use rust_os_playground::allocator;
+use rust_os_playground::memory::{self, BootInfoFrameAllocator};
+use rust_os_playground::println;
+use x86_64::VirtAddr;
 
 // Don't mangle function name (_start) - this is the entry point since
 // the linker looks for a function named `_start` by default. Update:
@@ -17,14 +23,44 @@ use rust_os_playground::{memory::BootInfoFrameAllocator, println};
 // point. Let’s rewrite our entry point function to use this macro:
 entry_point!(kernel_main);
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    rust_os_playground::init();
     println!("Hello World{}", "!");
 
+    rust_os_playground::init();
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
     let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
+
+    // Allocate a number on the heap
+    let x = Box::new(42);
+    println!("heap_value at {:p}", x);
+
+    // Create a dynamically sized vector
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+    println!("vec at {:p}", vec.as_slice());
+
+    // Create a reference counted vector -> will be freed when count reaches 0
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!(
+        "current reference count is {}",
+        Rc::strong_count(&cloned_reference)
+    );
+    core::mem::drop(reference_counted);
+    println!(
+        "reference count is {} now",
+        Rc::strong_count(&cloned_reference)
+    );
 
     #[cfg(test)]
     test_main();
 
+    println!("It did not crash!");
     rust_os_playground::hlt_loop();
 }
 
